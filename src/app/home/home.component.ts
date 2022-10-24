6ae67ae839eb7fecc9fe195460e4ca61a4da0156
Jose C Gomez <me@josecgomez.dev>
2022-10-24 19:16:25 -0400

    Lecture 15

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
 
+    
+  }
+  userName='Jose Gomez';
+  btnDisabled=true;
+
+  user ={
+    firstName:"Jose",
+    lastName:"Gomez",
+    emailAddress:"lol@lol.com"
+  }
+  myTimer:any=null;
   ngOnInit(): void {
+    this.myTimer=setTimeout(()=>{
+      alert('Stop this!');
+    },10000);
+  }
+
+  ngOnDestroy()
+  {
+    this.myTimer
+  }
+
+  DisplayHello(eventArgs:MouseEvent)
+  {
+    
+    console.log(eventArgs);
+    alert('Hello World');
+
+    console.log(this.user);
   }
 
 }
---
+++ src/app/secondhome/secondhome.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+
+@Component({
+  selector: 'app-secondhome',
+  templateUrl: './secondhome.component.html',
+  styleUrls: ['./secondhome.component.css']
+})
+export class SecondhomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
